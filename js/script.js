17f6c01019a6531e63da4c82aa8a9a0f4071e045
Westopw <westopw.bon@gmail.com>
2025-03-02 14:38:31 +0100

    First upload

---
+++ js/script.js
@@ -0,0 +1,369 @@
+var easy = ["Dieren", "Landen", "Pokemon", 
+            "Bekende mensen", "Fictionele karakters", "Bands & Artiesten", 
+            "Merken", "Drugs gerelateerd", "Wat in je broekzak past", 
+            "Wat je in de supermarkt kan tegenkomen", "Dingen die je lichaam ingaan", "Waar een kind in kan stikken", 
+            "Wat in je koelkast past", "Moordwapens",  "Engelse woorden", 
+            "Alles", "Alles voor 1 euro", "Alles voor 10 euro", 
+            "Wat je 10 meter ver kan gooien", "Waar je problemen mee krijgt bij de douane", "Waar je dood aan kan gaan", 
+            "Ik ga op reis en ik neem mee ...", "Alles dat niet bestaat", "Films & Series", "Muzieknummers", 
+            "Voertuigen", "Verzamelobjecten", "Vakantiebestemmingen", 
+            "Ik ga op vakantie en ik bezoek ...", "Godsdienst gerelateerd", "Alles waarvoor je gestraft wordt", 
+            "Game gerelateerd", "Winkels", "Sport gerelateerd", 
+            "Beroepen", "Waarvan je in het ziekenhuis beland als het vanaf twee verdiepingen op je land", "Scheldwoorden",
+            "Wat je niet in je ouder's slaapkamer wilt vinden", "Dingen die kut zijn", "Dingen die je met je moeder zou doen", 
+            "Jouw werk/studie gerelateerd", "Wat je kan eten", "Wat je niet kan eten", 
+            "Ziektes", "Flora", "Oorlogsgerelateerd", 
+            "Dingen die je kan stelen als je dronken bent", "Dingen die vet relaxt zijn", "Wetenschappelijk gerelateerd", 
+            "Ruimtevaart gerelateerd", "Dingen die je graag zou willen hebben", "Dingen die je met je baby zou doen", 
+            "Dingen harder dan een baksteen", "Dingen die je op 20 april zou doen", "Het eerste waar je aan denkt", 
+            "Bestaande of nieuwe omnibaap spel categoriën", "Dingen die vliegen", "Speelgoed", 
+            "Seks gerelateerd", "Dingen om een joint mee aan te steken", "Dingen die je kan drinken",
+            "Groentes", "Fruit", "Dingen die je niet in een vliegtuig mee kan nemen", 
+            "Kunst gerelateerd", "Luxeproducten", "Dingen om met een miljoen te doen", 
+            "Plekken om je wiet te bewaren", "Plekken om seks te hebben", "Dingen die drijven", 
+            "Boeken", "Catchphrases", "Culturen", 
+            "Steden", "Plekken", "Monsters", 
+            "Antagonisten", "Protagonisten", "Bouwmaterialen", 
+            "Dingen die geluid maken", "Voornamen", "Achternamen", 
+            "Electronica", "Politiek gerelateerd", "Stripfiguren", 
+            "Kantoorartikelen", "Oppervlaktes", "Memes", 
+            "Synoniemen voor 'joint'", "Woorden die al gezegd zijn", "Dingen die je in het stopcontact steekt", 
+            "Plekken om te zitten", "Lichaamsdelen", "Nostalgie", 
+            "Dingen die sus zijn", "Meubels", "Dingen om muzieknummers over te schrijven", 
+            "Decoraties", "Dingen die je niet ziet aankomen", "Fetishes", 
+            "Waarvan je in het ziekenhuis beland", "Snacks", "Dingen die je aan kan zetten",
+            "Snoepgoed", "Musicals", "Dingen die je uit kan zetten",
+            "Software", "Videogames", "Dingen om op te schrijven",
+            "Dingen om mee te schrijven", "Manieren om met moord weg te komen", "Redenen om op vakantie te gaan",
+            "Redenen om te protesteren", "Opleidingen", "Het beste idee van Nederland",
+            "Nummers om karaoke op te zingen", "Dromen", "Wapens voor tijdens een zombie apocalypse",
+            "Simon says...", "Beroemde mensen", "Lichamelijke aandoeningen",
+            "Openingszinnen", "Een nieuwe titel voor een boek", "Een nieuwe titel voor een muzieknummer",
+            "Een nieuwe uitvinding", "Namen voor een dating-app", "Dingen die heel klein zijn",
+            "Dingen die heel groot zijn", "Dingen die heel medium zijn", "Vrouwennamen",
+            "Mannennamen", "Genderloze voornamen", "Historische gebeurtenissen",
+            "Dingen om voor te bedelen", "Woorden om een kunstwerk mee te beschrijven", "Dingen waar je alles voor zou doen",
+            "Dingen waar je niets voor zou doen", "Dingen die je kan begraven", "Dingen met toetsen en knoppen",
+            "Plekken om je peuken te bewaren", "Dingen die je in de magnetron kan stoppen", "Dingen om in Minecraft te bouwen",
+            "Goede geuren voor een kaars", "Slechte geuren voor een kaars", "Dingen die je op je reis mee terug zou nemen",
+            "Dingen waar je vloeistof in kan doen", "Dingen om te doen tijdens het poepen", "Kinks",
+            "Plekken om te slapen", "Smokkelwaar", "Welkom bij mijn TED-talk over ...",
+            "Het laatste wat je doet", "Dingen die je kan stelen", "Dingen om naar iemand te gooien",
+            "Dingen om weg te gooien", "Dingen die je wilt hebben", "Absoluut het geld waard",
+            "Absoluut niet het geld waard", "Plekken om te wonen", "Dingen die trillen",
+            "Dingen zachter dan veren", "Dingen die zwaar zijn", "Dingen die licht zijn",
+            "Dingen die je niet in de magnetron kan stoppen"
+        ];
+
+var hard = ["Wat je niet aan kan raken", "Wat je in een kluis zou stoppen", "Wat mogelijk is na de dood", 
+            "Wat je niet kan zien", "Werkwoorden", "Dingen in een donkere steeg", 
+            "Muziekinstrumenten", "Trends voor 13-jarigen", "Wat jou 10 meter ver kan gooien", 
+            "Woordgrappen", "Afkortingen", "Wat je over je auto kan zeggen maar niet over je vriendin", 
+            "Palindroom", "Alles zonder Herman", "Een nieuw muziekgenre", 
+            "Kledingstukken",  "Engelse woorden, >=8 letters", "Onomatopeeën", 
+            "Schoonheidsproducten", "Seksstandjes", "Complottheoriën", 
+            "Ontbijt", "Kunststromingen", "Filmgenres",
+            "Kleuren", "Getallen", "Wetenschappers", 
+            "Kunstenaars", "Mythologische wezens", "Elke chillsessie ooit", 
+            "Bedrijfsmotto's", "Politieke partijen", "Goede doelen", 
+            "Toverspreuken", "Dingen die geen geluid maken", "Begroetingen", 
+            "Wereldleiders", "Wietsoorten", "Gebouwen", 
+            "Straatnamen", "Bordspellen", "Voorzetsels", 
+            "Dissen", "Organen", "Alles met Herman", 
+            "Feestdagen", "Kerst gerelateerd", "Halloween gerelateerd", 
+            "Nachtmerries", "Schoonmaakproducten", "Wat je over je vriendin kan zeggen maar niet over je auto", 
+            "Theesmaken", "Dingen die je kunt roken", "Kookgerei", 
+            "Alcoholische dranken", "Theatervoorstellingen", "Consoles",
+            "Nieuwjaarsvoornemens", "Recepten", "Sekstoys",
+            "Dingen uit de jaren 80", "Schoolvakken", "Eén van je drie magische wensen",
+            "Drankspellen", "Inside jokes", "Hollandse feestdagen",
+            "Games die veel beter zouden zijn in VR", "Knoppen op een rekenmachine", 
+            "Maanden in een jaar", "Dranken", "Dingen om een koekje in te dopen",
+            "Waar je een boek over zou kunnen schrijven", "Waar je een film over zou kunnen maken", "Namen voor knuffeldieren",
+            "Iets wat rijmt op het vorige woord", "Psychische aandoeningen", "Een nieuwe titel voor een wetenschappelijk artikel",
+            "Dingen die je lichaam uitkomen", "Talen en dialecten", "Redenen waarom lucifer uit de hemel is verbannen",
+            "Dingen die niet in een lift passen", "Muzieknummers met een niet-Engelse titel", "Dingen waar je in kan wonen",
+            "Handelingen die door AI vervangen kunnen worden", "De ideale schoonzoon", "Komt een vrouw bij de dokter...",
+            "Komt een man bij de dokter...", "Synoniemen voor geld", "Synoniemen voor bier",
+            "Kijk uit!...", "Verboden te...", "Dingen die je in de kelder kan vinden",
+            "Dingen die in een stopcontact passen", "Dingen met toeters en bellen", "Personages waar je een masker van wilt hebben",
+            "Dingen waar je een tattoo van zou willen hebben", "Waterlichamen", "Weddenschappen",
+            "Dingen waar je verslaafd aan kan raken", "Wie als eerst ..., wint!", "Criminelen",
+            "Leiderschapskwaliteiten", "Afko's", "Dikke mensen",
+            "Street names", "Namen voor een street gang", "Subculturen", 
+            "Dansmoves", "Outfits voor themafeesten", "Redenen om met iemand naar bed te gaan",
+            "Dingen die glibberig zijn", "Red flags", "Green flags",
+            "Wat je absoluut niet op je Tinder hebt willen staan", "Wat je absoluut niet op je LinkedIn wil hebben staan", "Redenen om te daten",
+            "Teleurstellingen", "Aangename verassingen", "One sentence horror story",
+            "Sitcoms", "Binge-waardige shows", "Een naam voor een IKEA-meubel",
+            "Namen voor een nieuw product", "Het onderwerp van je elevator pitch", "Domme manieren om dood te gaan",
+            "Wat je meeneemt naar een onbewoont eiland", "Dingen die je sociaal geaccepteerd mag stelen", "Dingen die niemand boeit",
+            "Dingen waar Adam Eva van had kunnen maken", "Ongemakkelijke situaties", "Kermisattracties",
+            "Pretparkattracties", "Thema's voor een pretpark", "Onhygiënische praktijken",
+            "Hygiënische praktijken", "Dingen waarvan je vind dat jij beter bent dan de rest", "Dingen om te doen tijdens het slaapwandelen",
+            ];
+
+var cleo = ["Dingen om in de BoBa te doen", "Dingen om in de BeBa te doen", "Dingen om in de TuBa te doen", 
+            "Redenen om bestuur te worden", "Boten", "Stammen",
+            "BeBa-hitjes", "Cleopatra gerelateerd", "Dingen die je met veilingitems kan doen",
+            "Cleopatranen", "Groningse uitgaansgelegenheden", "Verenigingen", 
+            "Thema's voor feesten", "Objecten om een klefemmer mee te geven", "BeBa-nummers waar je nu onderhand wel een beetje klaar mee bent",
+            "Dingen om tijdens een NICO te koken", "Manieren om 'kapitein' te zeggen", "Namen voor een boot",
+            "Namen voor een stam", "Dingen om op bootkamp te doen"
+            ];
+
+var log = [];
+var optionsout = 0;
+var prompt;
+var playing = [];
+var darkmode = 1;
+var cleopatrabutton = false;
+
+//easy - 0, hard - 1, all - 2
+var difficulty = 2;
+//default = 5
+var loglength = 5;
+//version
+var version = "v3.0_2025";
+
+$("#signature").append(version);
+
+//zet de categorieën klaar
+changedifficulty(difficulty);
+
+//de 'dubbel-checker'. Activeer om te checken of er dubbele waarden zijn.
+const frequency = (arr, item) => {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            count++;
+        }
+    }
+    return count;
+};
+
+for(let k = 0; k < playing.length; k++){
+console.log(frequency(playing, playing[k])); }
+//'dubbel-checker' uit
+
+
+//zet darkmode klaar (eerst local storage checken)
+if(window.localStorage.key("omnibaap_darkmodestorage") != null){
+darkmode = window.localStorage.getItem("omnibaap_darkmodestorage");}
+
+//1 = achtergrond zwart
+//0 = achtergrond wit
+
+if(darkmode == 1){
+    $("#helpbutton").css("border-color", "black");
+    $("#hamburger div").css("background-color", "black");
+    $("body").css("background-color", "black");
+    $("#prompt").css("color", "white");
+    $("#log").css("color", "rgb(170, 170, 170)");
+    $("#gradient").css("background-image", "linear-gradient(to bottom, rgba(255,0,0,0), 40%, black)");
+    $("#whiteblock").css("background-color", "black");
+    $("#button").attr("mode","dark");
+}else if(darkmode == 0){
+    $("#helpbutton").css("border-color", "white");
+    $("#hamburger div").css("background-color", "white");
+    $("body").css("background-color", "white");
+    $("#prompt").css("color", "black");
+    $("#log").css("color", "rgb(70, 70, 70)");
+    $("#gradient").css("background-image", "linear-gradient(to bottom, rgba(255,0,0,0), 40%, white)");
+    $("#whiteblock").css("background-color", "white");
+    $("#button").attr("mode","white");
+}//todo. Dit naar css overzetten
+
+//als hamburger geklikt
+$("#hamburger").on("click", function () {
+    
+    //check of al uitgeklapt of niet, afhankelijk daarvan vergroten/verkleinen
+    if(optionsout == 0){
+
+        $("#button").css("display", "none");
+        $("#hamburger div").css("background-color", "rgb(52, 78, 65)");
+        $("#options").css("transform", "translate(0vw, 0)");
+        $("header").css("border-radius", "0px");
+
+        optionsout = 1;
+    
+    }else if(optionsout == 1){
+    
+        $("#button").css("display", "inherit");
+        $("#options").css("transform", "translate(-100vw, 0)");
+        $("header").css("border-radius", "0 0 42px 42px");
+
+        if(darkmode == 0){
+            $("#hamburger div").css("background-color", "white");
+        }else if(darkmode == 1){
+            $("#hamburger div").css("background-color", "black");
+        }
+        optionsout = 0;
+    }
+
+});
+
+$("#cleopatrabutton").on("click", function () {
+    if(cleopatrabutton == false){
+        cleopatrabutton = true;
+        $("#cleopatrabutton").css("background-color", "rgb(88, 129, 87)");
+        $("#cleopatrabutton").css("border", "0.74vh solid rgb(58, 90, 64)");
+        $("#cleopatrabutton").css("margin-top", "1.48vh");
+        
+        //add to playing list
+        for(i = 0; i < cleo.length; i++){
+            playing.push(cleo[i]);
+        }        
+
+        console.log(playing);
+    }
+
+});
+
+//als helpknop geklikt ((in dit geval is dit nu de darkmode knop))
+$("#helpbutton").on("click", function () {
+    if(window.localStorage.key("omnibaap_darkmodestorage") != null){
+        darkmode = window.localStorage.getItem("omnibaap_darkmodestorage");}
+    if(darkmode == 0){ //maak zwart
+        $("#helpbutton").css("border-color", "black");
+        $("body").css("background-color", "black");
+        $("#prompt").css("color", "white");
+        $("#log").css("color", "rgb(170, 170, 170)");
+        $("#gradient").css("background-image", "linear-gradient(to bottom, rgba(255,0,0,0), 40%, black)");
+        $("#whiteblock").css("background-color", "black");
+        $("#button").attr("mode","black");
+
+        if(optionsout == 1){
+            $("#hamburger div").css("background-color", "rgb(52, 78, 65)");
+        }else if(optionsout == 0){
+            $("#hamburger div").css("background-color", "black");
+        }
+
+        //paste in local storage
+        window.localStorage.setItem("omnibaap_darkmodestorage", "1");
+        
+        darkmode = 1;
+
+    }else if(darkmode == 1){ //maak wit
+        $("#helpbutton").css("border-color", "white");
+        $("body").css("background-color", "white");
+        $("#prompt").css("color", "black");
+        $("#log").css("color", "rgb(70, 70, 70)");
+        $("#gradient").css("background-image", "linear-gradient(to bottom, rgba(255,0,0,0), 40%, white)");
+        $("#whiteblock").css("background-color", "white");
+        $("#button").attr("mode","white");
+        
+        if(optionsout == 1){
+            $("#hamburger div").css("background-color", "rgb(52, 78, 65)");
+        }else if(optionsout == 0){
+            $("#hamburger div").css("background-color", "white");
+        }
+
+        //paste in local storage
+        window.localStorage.setItem("omnibaap_darkmodestorage", "0");
+
+        darkmode = 0;
+
+    }//todo: dit overzetten naar css
+
+    console.log(darkmode);
+
+});
+
+//als button geklikt wordt
+$("#button").on("click", function () {
+
+    $("#button p").css("color", "rgb(255, 255, 255, 0%)");
+
+    printprompt(difficulty);
+});
+
+//functie voor het klikken van de difficulty knop
+function reply_clicked(difficultyclicked){
+
+    if(difficultyclicked != difficulty){
+        
+            //cleopatra setting resetten
+            $("#cleopatrabutton").css("background-color", "rgb(58, 90, 64)");
+            $("#cleopatrabutton").css("border", "none");
+            $("#cleopatrabutton").css("margin-top", "2.22vh");
+            cleopatrabutton = false;
+
+        difficulty = difficultyclicked;
+        changedifficulty(difficultyclicked);
+    }
+}
+
+//functie voor het veranderen van de difficulty
+function changedifficulty(difficulty) {
+         //create playing list
+         playing = [];
+         console.log(difficulty);
+
+         $(".active").toggleClass("active");
+         $(".difbutton#" + difficulty).toggleClass("active");
+
+         if(difficulty == 0 || difficulty == 2){
+            for(i = 0; i < easy.length; i++){
+                playing.push(easy[i]);
+            }     
+         }
+
+         if(difficulty == 1 || difficulty == 2){
+            for(i = 0; i < hard.length; i++){
+                playing.push(hard[i]);
+            }
+         }
+   
+         console.log(playing);   
+}
+
+//functie voor het kiezen en printen van de prompt
+function printprompt(difficulty) {
+    $("#button p").css("color", "rgb(255, 255, 255, 0%)");
+
+        random = Math.floor(Math.random() * playing.length);
+    
+        if($("#prompt").html() != ""){
+            
+            //reset log
+            $("#log").html("");
+
+            log.unshift(prompt);
+
+            if(log.length > loglength){
+                log.pop();
+            }
+
+            for(var i = 0; i < log.length; i++){
+                $("#log").append(log[i] + "<br/>");;
+            }
+        }
+        
+        if(playing.length == 1){
+            setTimeout(() => {
+                    playing = [];   
+
+                if(difficulty == 0 || difficulty == 2){
+                    for(i = 0; i < easy.length; i++){
+                        playing.push(easy[i]);
+                    }     
+                 }
+        
+                 if(difficulty == 1 || difficulty == 2){
+                    for(i = 0; i < hard.length; i++){
+                        playing.push(hard[i]);
+                    }
+                 }
+                    console.log(playing);
+                }, 100);
+        }
+
+                prompt = playing[random];
+
+                playing.splice(random, 1);
+
+                $("#prompt").html(prompt);
+    
+    console.log(playing.length);
+}
